07f0dc0018646ba8707898dfc36026ce07b03892
sergei_koriushov <lala170392@mail.ru>
2023-04-14 21:40:04 +0300

    1234

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
-    public static final String NUMBER = "12";
+    public static final String NUMBER = "1";
+    public static final String NUMBERS = "12";
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
